574b792823239243a5933a19e3cef64aff9bba3d
VitorMeloDev <vitormelodev@gmail.com>
2026-06-27 18:16:56 -0300

    Player Controller

---
+++ Module00/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody Rigidbody;
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float jumpForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Rigidbody.MovePosition(transform.position + m_Input * Time.deltaTime * speed);
+    }
+
+    void Jump()
+    {
+        Rigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+}
