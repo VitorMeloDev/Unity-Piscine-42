d12e67998d9d95b09de299a09e528856ef32ccde
VitorMeloDev <vitormelodev@gmail.com>
2026-06-28 13:59:08 -0300

    Update player controller: Jump check

--- Module01/Assets/Scripts/PlayerController.cs
+++ Module01/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private float x_axis;
-    private bool isActive = false;
+    private Collider collider;
+    private RaycastHit hit;
+
+    [SerializeField] private bool isActive = false;
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private LayerMask layerGround;
+
     [SerializeField] private float speed = 2;
     [SerializeField] private float jumpForce = 3;
+    [SerializeField] private float m_MaxDistance = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();    
+        collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && isActive)
+        isGrounded = Physics.CheckBox(
+            collider.bounds.center - Vector3.up * 0.55f,
+            new Vector3(
+                collider.bounds.extents.x * 0.9f,
+                0.05f,
+                collider.bounds.extents.z * 0.9f
+            ),
+            Quaternion.identity,
+            layerGround
+        );
+
+        if (!isActive)
+            return;
+        if (Input.GetButtonDown("Jump"))
             Jump();
     }
 
@@ -33,11 +53,34 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        if (isGrounded)
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
     public void SetActive(bool value)
     {
         isActive = value;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        //Check if there has been a hit yet
+        if (isGrounded)
+        {
+            //Draw a Ray forward from GameObject toward the hit
+            Gizmos.DrawRay(transform.position, (-transform.up) * hit.distance);
+            //Draw a cube that extends to where the hit exists
+            Gizmos.DrawWireCube(transform.position + (-transform.up) * hit.distance, transform.localScale);
+        }
+        //If there hasn't been a hit yet, draw the ray at the maximum distance
+        else
+        {
+            //Draw a Ray forward from GameObject toward the maximum distance
+            Gizmos.DrawRay(transform.position, (-transform.up) * m_MaxDistance);
+            //Draw a cube at the maximum distance
+            Gizmos.DrawWireCube(transform.position + (-transform.up) * m_MaxDistance, transform.localScale);
+        }
     }
 }
